7ac379fb37a67bf58ce0cb411cfa375426fc3df3
Tatani Herman <64686426+Tatani-Herman@users.noreply.github.com>
2022-02-08 15:04:49 +0100

    dernière version

--- src/main/java/com/project/coviddata/repositories/CovidDataRepository.java
+++ src/main/java/com/project/coviddata/repositories/CovidDataRepository.java
@@ -4,12 +4,29 @@ import com.project.coviddata.entities.CovidData;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.List;
+
+/**
+ * <p>
+ * Interface recensant les méthodes permettant d'interragir avec
+ * la base de données
+ * </p>
+ */
 
 @Repository
 public interface CovidDataRepository extends JpaRepository<CovidData,Integer> {
-
+    /**
+     * @param country
+     * @return la liste des données sur le covid pour un pays donné
+     */
     List<CovidData> findByPays(String country);
-    //List<CovidData> findByPaysAndDate(String country, String date);
-    CovidData findByPaysAndDate(String country, String date);
+
+    /**
+     * @param country
+     * @param date
+     * @return la lsite données sur le covid pour un pays donné et pour une date précise
+     */
+    List<CovidData> findByPaysAndDate(String country, LocalDate date);
 }
